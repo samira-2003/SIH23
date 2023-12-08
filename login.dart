428a4571b1ea729fc241f707dee3b2f06439d633
samira-2003 <141916686+samira-2003@users.noreply.github.com>
2023-12-08 12:45:14 +0530

    Add files via upload

---
+++ login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black,),
+    );
+  }
+}
